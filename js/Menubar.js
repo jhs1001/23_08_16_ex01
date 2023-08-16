ebed7ec7a9dc04d4d597bae10b38ee29f77522e8
jhs1001 <140129210+jhs1001@users.noreply.github.com>
2023-08-16 11:56:40 +0900

    main

---
+++ js/Menubar.js
@@ -0,0 +1,16 @@
+import { UIPanel } from './libs/ui.js';
+
+import { MenubarAdd } from './Menubar.Add.js';
+
+function Menubar( editor ) {
+
+	const container = new UIPanel();
+	container.setId( 'menubar' );
+
+	container.add( new MenubarAdd( editor ) );
+
+	return container;
+
+}
+
+export { Menubar };
